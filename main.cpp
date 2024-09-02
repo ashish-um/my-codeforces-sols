15aa9efa264cff75fa516e3a46e5c6b8e23dbf90
ashish-um <ashishumrao632005@gmail.com>
2024-09-02 22:04:30 +0530

    339A - Helpful Maths

--- main.cpp
+++ main.cpp
@@ -1,31 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char to_lowecase(char a){
-    if(int(a) >= 65 && int(a) <= 90){
-        return char(int(a)+32);
-    }
-    return a;
-}
-
 int main(){
-    // 112A - Petya and Strings 
-    
-    string a, b;
-    cin >> a >> b;
-    for(int i{}; i<a.length(); i++){
-        char A = to_lowecase(a[i]);
-        char B = to_lowecase(b[i]);
-
-        if(int(A) > int(B)){
-            cout << 1;
-            return 0;
+    // 339A - Helpful Maths 
+    string a; cin >> a;
+    vector<int> b;
+    for(char cha:a){
+        if(int(cha) != 43){
+            b.push_back(int(cha)-'0');
         }
-        if(int(A) < int(B)){
-            cout << -1;
-            return 0;
-        }
-
     }
-    cout << 0;
+
+    sort(b.begin(), b.end());
+
+    for(int i{}; i<b.size(); i++){
+        if(i == b.size()-1){
+            cout << b[i];
+        }else{
+            cout << b[i] << "+";
+        }
+    }
+
+    return 0;
 }
