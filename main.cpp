23eb9c43238018c76516195c9f0c730378fcf1e6
ashish-um <ashishumrao632005@gmail.com>
2024-09-02 13:13:11 +0530

    Next Round

--- main.cpp
+++ main.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 int main(){
-    //  Bit++
-    
-    int x{};
-    int t; cin >> t;
-    while(t--){
-        string n;
-        cin >> n;
-        for(int i{}; i<3; ++i){
-            if(n[i] == '+'){
-                x++;
-                break;
-            }
-            if(n[i] == '-'){
-                x--;
-                break;
-            }
+//    A. Next Round 
+    int n,k; cin >> n >> k;
+    int res{};
+    int pos=1;
+    int k_val = -1;
+    while(n--){
+        int a;
+        cin >> a;
+        if(pos == k) k_val = a;
+        if(k_val > 0 && a < k_val){
+            cout << res;
+            return 0;
         }
+        if(a != 0){
+            ++res;
+        }
+        ++pos;
     }
-    cout << x;
-    return 0;
+    cout << res;
 }
