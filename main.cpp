d7bd09354b2557621fa1840e033b79262924c785
ashish-um <ashishumrao632005@gmail.com>
2024-09-03 12:00:00 +0530

    432A - Choosing Teams

--- main.cpp
+++ main.cpp
@@ -2,46 +2,14 @@
 using namespace std;
 
 int main(){
-
-    // 490A - Team Olympiad 
-
-    int n; cin >> n;
-    vector<int> a(n);
-    unordered_map<int,vector<int>> me;
-    int min=-1;
-
-    //input
+//  432A - Choosing Teams 
+    int n,k; cin >> n >> k;
+    int count{};
     for(int i{}; i<n; i++){
-        cin >> a[i];
-        me[a[i]].push_back(i+1);
-    }
-
-    if(me.size() < 3){
-        cout << 0;
-        return 0;
-
-    }
-
-    for(auto it:me){
-        min = fmin(min, it.second.size());
-        if(min == -1) min = it.second.size();
-    }
-
-    vector<vector<int>> res(min);
-    cout << min << endl;
-
-    for(auto it:me){
-        for(int i{}; i<min; i++){
-            res[i].push_back(it.second[i]);
+        int a; cin >> a;
+        if(a+k <= 5){
+            count++;
         }
     }
-
-    // output
-    for(vector<int> a:res){
-        for(int b:a){
-            cout << b << " ";
-        }
-        cout << endl;
-    }
-
+    cout << count/3;
 }
