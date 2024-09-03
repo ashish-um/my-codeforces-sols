ea04774b3f4886997a336c109c2006e281d3e233
ashish-um <ashishumrao632005@gmail.com>
2024-09-03 11:17:17 +0530

    490A - Team Olympiad

--- main.cpp
+++ main.cpp
@@ -2,15 +2,46 @@
 using namespace std;
 
 int main(){
-    //  791A - Bear and Big Brother 
 
-    int a,b; cin >> a >> b;
-    int count=0;
-    while(b>=a){
-        b*=2;
-        a*=3;
-        count++;
+    // 490A - Team Olympiad 
+
+    int n; cin >> n;
+    vector<int> a(n);
+    unordered_map<int,vector<int>> me;
+    int min=-1;
+
+    //input
+    for(int i{}; i<n; i++){
+        cin >> a[i];
+        me[a[i]].push_back(i+1);
     }
 
-    cout << count;
+    if(me.size() < 3){
+        cout << 0;
+        return 0;
+
+    }
+
+    for(auto it:me){
+        min = fmin(min, it.second.size());
+        if(min == -1) min = it.second.size();
+    }
+
+    vector<vector<int>> res(min);
+    cout << min << endl;
+
+    for(auto it:me){
+        for(int i{}; i<min; i++){
+            res[i].push_back(it.second[i]);
+        }
+    }
+
+    // output
+    for(vector<int> a:res){
+        for(int b:a){
+            cout << b << " ";
+        }
+        cout << endl;
+    }
+
 }
