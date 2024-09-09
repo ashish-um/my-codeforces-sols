67d54824dfea14327c54bc99efc27b4b48882027
ashish-um <ashishumrao632005@gmail.com>
2024-09-09 19:06:39 +0530

    122A - Lucky Division

--- main.cpp
+++ main.cpp
@@ -1,28 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 
-int main(){
-// 1881A - Don't Try to Count 
 
-   int t; cin >> t;
-   while(t--){
-      int n, m; cin >> n >> m;
 
-      string x, s;
-      cin >> x >> s;
-      int tries{};
-      for(int i{}; i < 10; i++){
-         if(x.find(s) != x.npos){
-            break;
-         }
-         x += x;
-         tries++;
-      }
-      if(x.find(s) != x.npos){
-         cout << tries << endl;
-      }else{
-         cout << -1 << endl;
-      }
-      
+vector<int> gen_vec(int c, int len){
+   vector<int> res;
+   for(int i{}; i<len; i++){
+      if(i<c) res.push_back(4);
+      else res.push_back(7);
    }
+   return res;
+}
+
+signed main(){
+   int n; cin>>n;
+   for(int i=1; i<=to_string(n).length(); i++){
+      // i = digits;
+      for(int j{}; j<=i; j++){
+         vector<int> my_vec = gen_vec(j, i);
+
+         do{
+            string num_str{};
+            for(int elem:my_vec){
+               num_str += to_string(elem);
+            }
+            int num = stoi(num_str);
+            if(n%num == 0){
+               cout << "YES";
+               return 0;
+            }
+         }while (next_permutation(my_vec.begin(), my_vec.end()));
+      }
+
+   }
+   cout << "NO";
 }
