8d1c0ae5d1c19b2fdaf836d72615aa28e50283c1
ashish-um <ashishumrao632005@gmail.com>
2024-09-10 13:23:30 +0530

    313A - Ilya and Bank Account

--- main.cpp
+++ main.cpp
@@ -2,37 +2,26 @@
 using namespace std;
 #define int long long
 
-
-
-vector<int> gen_vec(int c, int len){
-   vector<int> res;
-   for(int i{}; i<len; i++){
-      if(i<c) res.push_back(4);
-      else res.push_back(7);
-   }
-   return res;
-}
-
 signed main(){
-   int n; cin>>n;
-   for(int i=1; i<=to_string(n).length(); i++){
-      // i = digits;
-      for(int j{}; j<=i; j++){
-         vector<int> my_vec = gen_vec(j, i);
+   // 313A - Ilya and Bank Account 
+   int n; cin >> n;
 
-         do{
-            string num_str{};
-            for(int elem:my_vec){
-               num_str += to_string(elem);
-            }
-            int num = stoi(num_str);
-            if(n%num == 0){
-               cout << "YES";
-               return 0;
-            }
-         }while (next_permutation(my_vec.begin(), my_vec.end()));
-      }
-
+   if(n>0) {
+      cout << n;
+      return 0;
    }
-   cout << "NO";
+   int l = n%10;
+   int l2 = (n%100)/10;
+
+   string reduced = to_string(n/100);
+
+   if(to_string(n).length() <= 3){
+      int res = stoi(to_string(fmax(l,l2)));
+      cout << res;
+   }else{
+      int res = stoi(reduced+to_string((int)fmax(l,l2)*-1));
+
+      cout << res;
+   }
+
 }
