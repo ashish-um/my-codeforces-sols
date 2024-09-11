33ee6e42767dfe4330be05ef8cd430278553f499
ashish-um <ashishumrao632005@gmail.com>
2024-09-11 14:13:43 +0530

    1374B - Multiply by 2, divide by 6

--- main.cpp
+++ main.cpp
@@ -3,25 +3,20 @@ using namespace std;
 #define int long long
 
 signed main(){
-   // 313A - Ilya and Bank Account 
-   int n; cin >> n;
-
-   if(n>0) {
-      cout << n;
-      return 0;
+   // 1374B - Multiply by 2, divide by 6 
+   int t; cin >> t;
+   while(t--){
+      int n; cin >>n;
+      int i{};
+      while(n%6 == 0 || n%3 == 0){
+         if(n%6==0){
+            n/=6;
+         }else{
+            n*=2;
+         }
+         i++;
+      }
+      if(n==1) cout << i << endl;
+      else cout << -1 << endl;
    }
-   int l = n%10;
-   int l2 = (n%100)/10;
-
-   string reduced = to_string(n/100);
-
-   if(to_string(n).length() <= 3){
-      int res = stoi(to_string(fmax(l,l2)));
-      cout << res;
-   }else{
-      int res = stoi(reduced+to_string((int)fmax(l,l2)*-1));
-
-      cout << res;
-   }
-
 }
