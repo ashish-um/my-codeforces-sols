b681488b7a9a2f17714a1f9fd6c1e36b3ccd214b
ashish-um <ashishumrao632005@gmail.com>
2024-09-11 14:43:34 +0530

    1475B - New Year's Number

--- main.cpp
+++ main.cpp
@@ -3,20 +3,23 @@ using namespace std;
 #define int long long
 
 signed main(){
-   // 1374B - Multiply by 2, divide by 6 
+// 1475B - New Year's Number 
+
    int t; cin >> t;
    while(t--){
-      int n; cin >>n;
-      int i{};
-      while(n%6 == 0 || n%3 == 0){
-         if(n%6==0){
-            n/=6;
-         }else{
-            n*=2;
+      int n; cin >> n;
+      int n2020 = n, n2021 = n;
+
+      while(n2020%2021 != 0 && n2021%2020 != 0){
+         n2020 -= 2020;
+         n2021 -= 2021;
+         n = fmin(n2020,n2021);
+         if(n < 2020) {
+            break;
          }
-         i++;
       }
-      if(n==1) cout << i << endl;
-      else cout << -1 << endl;
+      if(n<2020){
+         cout << "NO" << endl;
+      }else cout << "YES" << endl;
    }
 }
