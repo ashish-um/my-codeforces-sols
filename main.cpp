f67fe7f154e2d1ce2ea1bbb816d39a15a88bcaec
ashish-um <ashishumrao632005@gmail.com>
2024-09-17 21:53:49 +0530

    26A - Almost Prime

--- main.cpp
+++ main.cpp
@@ -2,28 +2,58 @@
 using namespace std;
 #define int long long
 
-signed main(){
-   // 1807D - Odd Queries 
-   int t; cin >> t;
-   while(t--){
-      int n,q; cin >> n >> q;
-      vector<int> arr(n+1);
-      vector<int> sums(n+1);
-      for(int i=1; i<=n; i++) {
-         cin >> arr[i];
-         sums[i] = sums[i-1]+arr[i];
+vector<int> sieve(){
+   int num = 3000;
+   vector<bool> v(num+1);
+   vector<bool> prev;
+   int curr = 2;
+   while(prev != v) {
+      prev = v;
+      for(int i=curr*curr; i<=num; i+=curr){
+         v[i] = 1; // 1 not prime
       }
-
-      while(q--){
-         int l,r,k; cin >> l >> r >> k;
-         int sum = sums[n] - sums[r] + sums[l-1] + k*(r-l+1);
-
-         if(sum%2==0){
-            cout << "NO";
-         }else{
-            cout << "YES";
+      for(int n=curr+1; n<=num; n++){
+         if(v[n] == 0){
+            curr = n;
+            break;
          }
-         cout << endl;
       }
    }
+
+   vector<int> res;
+   for(int i=2; i<=num; i++){
+      if(v[i] == 0){
+         // cout << i << " ";
+         res.push_back(i);
+      }
+   }
+   return res;
+
+}
+
+signed main(){
+   // 26A - Almost Prime 
+   int n; cin >> n;
+
+   vector<int> v = sieve();
+
+   int CuntRes{};
+   for(int i=6; i<=n; i++){
+      int num = 0;
+      int count{};
+      while(v[num]<n && num < v.size()){
+         if(i%v[num]==0){
+            count++;
+            if(count > 2){
+               break;
+            }
+         }
+         num++;
+      }
+
+      if(count == 2){
+         CuntRes++;
+      }
+   }
+   cout << CuntRes;
 }
