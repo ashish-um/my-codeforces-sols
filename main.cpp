4531b918b16e8e190cba90f0832fb8fba00a1bed
ashish-um <ashishumrao632005@gmail.com>
2024-09-18 14:19:02 +0530

    A - Lights Out

--- main.cpp
+++ main.cpp
@@ -2,58 +2,26 @@
 using namespace std;
 #define int long long
 
-vector<int> sieve(){
-   int num = 3000;
-   vector<bool> v(num+1);
-   vector<bool> prev;
-   int curr = 2;
-   while(prev != v) {
-      prev = v;
-      for(int i=curr*curr; i<=num; i+=curr){
-         v[i] = 1; // 1 not prime
-      }
-      for(int n=curr+1; n<=num; n++){
-         if(v[n] == 0){
-            curr = n;
-            break;
-         }
-      }
-   }
-
-   vector<int> res;
-   for(int i=2; i<=num; i++){
-      if(v[i] == 0){
-         // cout << i << " ";
-         res.push_back(i);
-      }
-   }
-   return res;
-
-}
-
 signed main(){
-   // 26A - Almost Prime 
-   int n; cin >> n;
+   // A - Lights Out 
+   vector<vector<bool>> v(5, vector<bool>(5, true));
 
-   vector<int> v = sieve();
-
-   int CuntRes{};
-   for(int i=6; i<=n; i++){
-      int num = 0;
-      int count{};
-      while(v[num]<n && num < v.size()){
-         if(i%v[num]==0){
-            count++;
-            if(count > 2){
-               break;
-            }
-         }
-         num++;
-      }
-
-      if(count == 2){
-         CuntRes++;
+   for(int i=1; i<=3; i++){
+      for(int j=1; j<=3; j++){
+         int inp; cin >> inp;
+         v[i][j] = (inp%2!=0 ? !v[i][j] : v[i][j]);
+         v[i-1][j] = (inp%2!=0 ? !v[i-1][j] : v[i-1][j]);
+         v[i+1][j] = (inp%2!=0 ? !v[i+1][j] : v[i+1][j]);
+         v[i][j-1] = (inp%2!=0 ? !v[i][j-1] : v[i][j-1]);
+         v[i][j+1] = (inp%2!=0 ? !v[i][j+1] : v[i][j+1]);
       }
    }
-   cout << CuntRes;
+
+   for(int i=1; i<=3; i++){
+      for(int j=1; j<=3; j++){
+         cout << v[i][j];
+      }
+      cout << endl;
+   }
+
 }
