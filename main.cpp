86ed3fcabb326862f3e89e65d514dd0a7aafe165
ashish-um <ashishumrao632005@gmail.com>
2024-09-20 01:02:39 +0530

    1992C - Gorilla and Permutation

--- main.cpp
+++ main.cpp
@@ -3,38 +3,19 @@ using namespace std;
 #define int long long
 
 signed main(){
+   //  1992C - Gorilla and Permutation 
    int t;
    cin >> t;
 
    while(t--){
-      int n,k; cin >> n >> k;
-      string s; cin >> s;
-
-      unordered_map<char, int> m;
-
-      for(char c:s){
-         m[c]++;
-      }
-
-      int even{}, odd{};
-      for(auto it:m){
-         if((float)it.second/2>0){
-            even += (it.second/2)*2;
-            odd += it.second%2;
+      int n,m,k; cin >> n >> m >> k;
+      for(int i=n; i>0; i--){
+         if(i-m<=0){
+            cout << m-i+1 << " ";
          }else{
-            odd += it.second;
+            cout << i << " ";
          }
-      }
-
-      int diff = odd-k;
-
-      if(diff<2){
-         if(diff < 0){
-            if(even + diff > 0) cout << "YES";
-            else cout << "NO";
-         } else cout << "YES";
-      }else{
-         cout << "NO";
+         
       }
       cout << endl;
    }
