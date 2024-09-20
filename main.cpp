5ae2ca83539784d83fc1c4e03408f389a08867b8
ashish-um <ashishumrao632005@gmail.com>
2024-09-20 16:36:52 +0530

    B - Decoding

--- main.cpp
+++ main.cpp
@@ -3,21 +3,19 @@ using namespace std;
 #define int long long
 
 signed main(){
-   //  1992C - Gorilla and Permutation 
-   int t;
-   cin >> t;
+   int n; cin >> n;
+   string s; cin >> s;
+   int median = s.length()/2;
 
-   while(t--){
-      int n,m,k; cin >> n >> m >> k;
-      for(int i=n; i>0; i--){
-         if(i-m<=0){
-            cout << m-i+1 << " ";
-         }else{
-            cout << i << " ";
-         }
-         
+   string res = (s.length()%2==0 ? "" : string(1,s[median]));
+   for(int i=1; i<=median; i++){
+      if(s.length()%2==0){
+         res += (string(1, s[median-i]) + string(1, s[median+i-1]));
+
+      }else{
+         res += (string(1, s[median-i]) + string(1, s[median+i]));
+
       }
-      cout << endl;
    }
-   
+   cout << res;
 }
