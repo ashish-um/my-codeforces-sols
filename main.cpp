6dffac93e8743b6e2f7b37adb48d566333885ceb
ashish-um <ashishumrao632005@gmail.com>
2024-09-21 02:36:04 +0530

    A - Mainak and Array

--- main.cpp
+++ main.cpp
@@ -3,19 +3,26 @@ using namespace std;
 #define int long long
 
 signed main(){
-   // A. Lunch Rush
-   int n,k; cin >> n >> k;
-   int max=-LONG_MAX;
-   while(n--){
-      int f,t; cin >> f >> t;
-      if(t>k){
-         max = fmax(max, (f-(t-k)));
-
-      }else{
-         max = fmax(max, f);
+   int t; cin >> t;
+   while(t--){
+      // A - Mainak and Array 
+      int n; cin >> n;
+      vector<int> v(n);
+      int max= -LONG_MAX;
+      int min= LONG_MAX;
+      int maxNearDiff = -LONG_MAX;
+      for(int i{}; i<n; i++){
+         cin >> v[i];
+         max = fmax(max, v[i]);
+         min = fmin(min, v[i]);
+         if(i>=1 && i<n){
+            maxNearDiff = fmax(maxNearDiff, v[i-1]-v[i]);
+         }
 
       }
-   }
+      int currMax = fmax(max - v[0], *v.rbegin() - min);
 
-   cout << max;
+
+      cout << fmax(maxNearDiff, currMax) << endl;
+   }
 }
