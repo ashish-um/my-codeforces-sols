e6befc758de6ceaad92171dce33352f05e71cbac
ashish-um <ashishumrao632005@gmail.com>
2024-09-21 10:12:23 +0530

    B - Not Dividing

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define cout(v) for(auto i:v){cout<<i<<" ";}cout<<endl;
+#define cin(v) for(auto &i:v){cin>>i;}
 
 signed main(){
+// B - Not Dividing 
+
    int t; cin >> t;
    while(t--){
-      // A - Mainak and Array 
       int n; cin >> n;
       vector<int> v(n);
-      int max= -LONG_MAX;
-      int min= LONG_MAX;
-      int maxNearDiff = -LONG_MAX;
+      // cin(v);
       for(int i{}; i<n; i++){
          cin >> v[i];
-         max = fmax(max, v[i]);
-         min = fmin(min, v[i]);
-         if(i>=1 && i<n){
-            maxNearDiff = fmax(maxNearDiff, v[i-1]-v[i]);
+         if(v[i] == 1){
+            v[i]++;
          }
-
       }
-      int currMax = fmax(max - v[0], *v.rbegin() - min);
 
+      // int steps{};
+      for(int i=1; i<n; i++){
+         if(v[i]%v[i-1] == 0){
+            v[i]++;
+            // i=0;
+         }
+      }
 
-      cout << fmax(maxNearDiff, currMax) << endl;
+      cout(v);
+      // cout << "steps: " << steps << "\n";
    }
 }
