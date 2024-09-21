f26dafd02c82c563bdf9c74eaf12bc4cd19a89da
ashish-um <ashishumrao632005@gmail.com>
2024-09-21 13:03:01 +0530

    B - Construct the String

--- main.cpp
+++ main.cpp
@@ -1,18 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
+// #define int long long
 #define cout(v) for(auto i:v){cout<<i<<" ";}cout<<endl;
 #define cin(v) for(auto &i:v){cin>>i;}
 
-signed main(){
-   // 460A Vasya and Socks 
-   int n, m; cin >> n >> m;
-
-   int diff = n/m;
-   while(diff > 0){
-      int exrtaC = n/m;
-      n += diff;
-      diff = (n/m) - exrtaC;
+char nextChar(char c){
+   if(c == 'z'){
+      return 'a';
    }
-   cout << n;
+   return char(int(c)+1);
+}
+
+signed main(){
+   //  B - Construct the String 
+   int t; cin >> t;
+   while(t--){
+      int n, a, b; cin >> n >> a >> b;
+
+      string s;
+      char start = 'z';
+      int dist = b;
+
+      
+      for(int i=1; i<=a; i++){
+         if(dist > 0){
+            dist--;
+            s += nextChar(start);
+            start = nextChar(start);
+         }else{
+            s += start;
+         }
+      }
+
+      for(int u=2; u<=n-a+1; u++){
+         unordered_set<char> ms;
+         for(int i=u-1; i < s.length(); i++){
+            ms.insert(s[i]);
+         }
+         dist = ms.size();
+         if(b-dist==0){
+            s+=start;
+         }else{
+            s+=nextChar(start);
+            start = nextChar(start);
+         }
+      }
+
+      cout << s << endl;
+
+   }
 }
