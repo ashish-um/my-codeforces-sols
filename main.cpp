243dfebfa059af813c8aea37d96cd788044aeac8
ashish-um <ashishumrao632005@gmail.com>
2024-09-21 11:58:56 +0530

    460A Vasya and Socks

--- main.cpp
+++ main.cpp
@@ -5,29 +5,14 @@ using namespace std;
 #define cin(v) for(auto &i:v){cin>>i;}
 
 signed main(){
-// B - Not Dividing 
+   // 460A Vasya and Socks 
+   int n, m; cin >> n >> m;
 
-   int t; cin >> t;
-   while(t--){
-      int n; cin >> n;
-      vector<int> v(n);
-      // cin(v);
-      for(int i{}; i<n; i++){
-         cin >> v[i];
-         if(v[i] == 1){
-            v[i]++;
-         }
-      }
-
-      // int steps{};
-      for(int i=1; i<n; i++){
-         if(v[i]%v[i-1] == 0){
-            v[i]++;
-            // i=0;
-         }
-      }
-
-      cout(v);
-      // cout << "steps: " << steps << "\n";
+   int diff = n/m;
+   while(diff > 0){
+      int exrtaC = n/m;
+      n += diff;
+      diff = (n/m) - exrtaC;
    }
+   cout << n;
 }
