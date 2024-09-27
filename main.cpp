ab98f3d0215dd2d6fc46489ee366d65bc9846fd0
ashish-um <ashishumrao632005@gmail.com>
2024-09-27 15:32:21 +0530

    131A - cAPS lOCK

--- main.cpp
+++ main.cpp
@@ -1,52 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 // #define int long long
-#define cout(v) for(auto i:v){cout<<i<<" ";}cout<<endl;
+#define cout(v) for(auto i:v){cout<<i<<" ";}cout<<endl
 #define cin(v) for(auto &i:v){cin>>i;}
-
-char nextChar(char c){
-   if(c == 'z'){
-      return 'a';
-   }
-   return char(int(c)+1);
-}
+#define qsort(v) sort(v.begin(), v.end())
 
 signed main(){
-   //  B - Construct the String 
-   int t; cin >> t;
-   while(t--){
-      int n, a, b; cin >> n >> a >> b;
-
-      string s;
-      char start = 'z';
-      int dist = b;
-
-      
-      for(int i=1; i<=a; i++){
-         if(dist > 0){
-            dist--;
-            s += nextChar(start);
-            start = nextChar(start);
-         }else{
-            s += start;
-         }
+   // 131A cAPS lOCK 
+   string s; cin >> s;
+   int upper{};
+   for(char c:s){
+      if(int(c) >= 65 && int(c)<=90){
+         upper++;
       }
-
-      for(int u=2; u<=n-a+1; u++){
-         unordered_set<char> ms;
-         for(int i=u-1; i < s.length(); i++){
-            ms.insert(s[i]);
-         }
-         dist = ms.size();
-         if(b-dist==0){
-            s+=start;
-         }else{
-            s+=nextChar(start);
-            start = nextChar(start);
-         }
-      }
-
-      cout << s << endl;
-
    }
+
+   string n;
+   if(upper == s.length()){
+      for(char c:s){
+         n += char(int(c)+32);
+      }
+   }else if(upper == s.length()-1 && s[0]>=97 && s[0]<=122){
+      char first = s[0];
+      for(char c:s){
+         n += char(int(c)+32);
+      }
+      n[0] = char(int(first)-32);
+   }
+   else{
+      n = s;
+   }
+   cout << n;
 }
