b872e0ba608a5409f7d28f2a9cb32e6649248a1c
ashish-um <ashishumrao632005@gmail.com>
2024-09-29 15:07:56 +0530

    1353C - Board Moves

--- main.cpp
+++ main.cpp
@@ -1,34 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-// #define int long long
+#define int long long
 #define cout(v) for(auto i:v){cout<<i<<" ";}cout<<endl
 #define cin(v) for(auto &i:v){cin>>i;}
 #define qsort(v) sort(v.begin(), v.end())
 
 signed main(){
-   // 131A cAPS lOCK 
-   string s; cin >> s;
-   int upper{};
-   for(char c:s){
-      if(int(c) >= 65 && int(c)<=90){
-         upper++;
+   //  1353C - Board Moves  
+   int t; cin >> t;
+   while(t--){
+      int n; cin >> n;
+      int res{};
+      for(int i=3,multipler=1; i<=n; i+=2, multipler++){
+         res += ((i*4)-4)*multipler;
       }
+      cout << res << endl;
    }
-
-   string n;
-   if(upper == s.length()){
-      for(char c:s){
-         n += char(int(c)+32);
-      }
-   }else if(upper == s.length()-1 && s[0]>=97 && s[0]<=122){
-      char first = s[0];
-      for(char c:s){
-         n += char(int(c)+32);
-      }
-      n[0] = char(int(first)-32);
-   }
-   else{
-      n = s;
-   }
-   cout << n;
 }
