b43c3c13d7de5381b445e01c0cf55f6555022a4e
ashish-um <ashishumrao632005@gmail.com>
2024-10-09 00:50:50 +0530

    1829C - Mr. Perfectly Fine

--- main.cpp
+++ main.cpp
@@ -6,29 +6,36 @@ using namespace std;
 #define qsort(v) sort(v.begin(), v.end())
 #define all(v) v.begin(), v.end()
 
-bool doCounter(int bit, int index){
-    return (((bit>>index)&1)==1);
-}
-
 signed main(){
-    // 1097B - Petr and a Combination Lock 
-    int n; cin >> n;
-    vector<int> v(n); cin(v);
+    // 1829C - Mr. Perfectly Fine 
+    int t; cin>>t;
+    while(t--){
 
-    for(int i{}; i<(1<<n); i++){
-        int sum{};
-        for(int j{}; j<n; j++){
-            if(doCounter(i, j)){
-                sum += v[j];
-            }else sum -= v[j];
+        int p; cin >> p;
+        int mamin=LONG_MAX;
+
+        vector<int> L,R;
+        int l=-1,r=-1;
+
+        while(p--){
+            int m; cin >> m;
+            string bit; cin >> bit;
+            if(bit == "10") {
+                if(l==-1) l = m;
+                else l = min(l, m);
+            }
+            if(bit == "01") {
+                if(r==-1) r=m;
+                else r = min(r, m);
+            }
+
+            if(l>0 && r>0)
+            mamin = min(mamin, (l+r));
+            if(bit == "11") mamin = min(mamin, m);
         }
-        sum = (sum%360);
-        if(sum == 0){
-            cout << "YES";
-            return 0;
-        }
+
+        if(mamin == LONG_MAX){
+            cout << -1 << endl;
+        }else cout << mamin << endl;
     }
-
-    cout << "NO";
-
 }
