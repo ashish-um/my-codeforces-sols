8cb4ad5a51d934f1a24f9c3375e36fc5eee0d442
ashish-um <ashishumrao632005@gmail.com>
2024-10-10 03:02:47 +0530

    B - Rock and Lever

--- main.cpp
+++ main.cpp
@@ -6,24 +6,27 @@ using namespace std;
 #define qsort(v) sort(v.begin(), v.end())
 #define all(v) v.begin(), v.end()
 
+int msb(int n){
+    return 63 - __builtin_clzll(n);
+}
+
 signed main(){
-    int n; cin >> n;
-    vector<int>v(5);
-    for(int i{}; i<n;i++){
-        int inp; cin >> inp;
-        v[inp]++;
+    // B - Rock and Lever 
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vector<int>v(n); cin(v);
+
+        unordered_map<int, int> m;
+        for(int i:v){
+            int ms = msb(i)-1;
+            m[ms]++;
+        }
+
+        int res{};
+        for(auto &[x, y]:m){
+            res += (y*(y-1))/2;
+        }
+        cout << res << endl;
     }
-
-    int req = v[4] + v[3] + v[2]/2 + v[2]%2;
-
-    v[1] = (v[1]-v[3]);
-    v[1] = v[1] - (v[2]%2)*2;
-
-    if(v[1]>4) {
-        req += v[1]/4 + (v[1]%4>0?1:0);
-    }else if(v[1]>0){
-        req += 1;
-    }
-
-    cout << req;
 }
