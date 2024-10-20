5f8efcee4a71e0bcfffb7c6d51b6b25b4d4bef07
ashish-um <ashishumrao632005@gmail.com>
2024-10-20 15:14:08 +0530

    1399B - Gifts Fixing

--- test.cpp
+++ test.cpp
@@ -6,46 +6,32 @@ using namespace std;
 #define qsort(v) sort(v.begin(), v.end())
 #define all(v) v.begin(), v.end()
 
-signed main(){
-    // 2005B1 - The Strict Teacher (Easy Version) 
-    
-    int t; cin >>t;
-    while(t--){
-        int n,m,q;cin>>n>>m>>q;
-        vector<int> teachers(m);
-        cin(teachers);
+void solve(){
+    //  1399B - Gifts Fixing 
+    int n; cin >> n;
+    vector<int> a(n); cin(a);
+    vector<int> b(n); cin(b);
 
-        for(int i{}; i<q; i++){
-            int david; cin >> david;
+    int aMiN = *min_element(all(a));
+    int bMiN = *min_element(all(b));
 
-            int posMIN = LONG_MAX, negMIN = LONG_MIN;
-            int minElem = LONG_MAX;
-            int maxElem = LONG_MIN;
-            for(int j{}; j<m; j++){
-                if(teachers[j] - david > 0){ // POS DIFF
-                    posMIN = min(posMIN, teachers[j]-david);
-                }else{                      // NEG DIFF
-                    negMIN = max(negMIN, teachers[j]-david);
-                }
-                minElem = min(minElem, teachers[j]);
-                maxElem = max(maxElem, teachers[j]);
-            }
-
-            if(negMIN == 0){
-                cout << 0;
-            }
-            else if(negMIN == LONG_MIN){
-                cout << minElem-1;
-            }
-            else if(posMIN == LONG_MAX){
-                cout << n-maxElem;
-
-            }else{
-                int mid = (negMIN + posMIN)/2;
-                cout << min(posMIN-mid, abs(negMIN-mid));
-            }
-            cout << "\n";
+    int res{};
+    for(int i{}; i<n; i++){
+        if(a[i]!=aMiN && b[i]!=bMiN){
+            int m = max(a[i]-aMiN, b[i]-bMiN);
+            res += m;
+        }else{
+            res += (a[i]-aMiN) + (b[i]-bMiN);
         }
     }
 
+    cout << res << "\n";
+}
+
+
+signed main(){
+    int t; cin >> t;
+    while(t--){
+       solve();
+    }
 }
