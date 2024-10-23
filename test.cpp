7b98110f721e6e3340aadc413d27ac4def254b16
ashish-um <ashishumrao632005@gmail.com>
2024-10-24 02:47:32 +0530

    489C - Given Length and Sum of Digits...

--- test.cpp
+++ test.cpp
@@ -1,36 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define cout(v) for(auto i:v){cout<<i<<" ";}cout<<endl
+#define cout(v) for(auto i:v){cout<<i<<"";}
 #define cin(v) for(auto &i:v){cin>>i;}
 #define qsort(v) sort(v.begin(), v.end())
 #define all(v) v.begin(), v.end()
 
 void solve(){
-    int n, m; cin >> n >> m;
-    vector<int> v(n); cin(v);
-    int maax = *max_element(all(v));
-    while(m--){
-        char oper; cin >> oper;
-        int l,r; cin >> l >> r;
+    // 489C - Given Length and Sum of Digits
+    int m,s; cin>>m>>s;
+    if(m == 0 || s>9*m){
+        cout<<"-1 -1";
+        return;
+    }
 
-        if(maax>=l && maax <= r){
-            if(oper == '+'){
-                maax++;
-            }else{
-                maax--;
+    if(m==1 && s==0){
+        cout << "0 0";
+        return;
+    }
+
+    if(m > 0 && s==0){
+        cout << "-1 -1";
+        return;
+    }
+    
+    vector<int> v(m);
+    int sum = s;
+
+    for(int i=m-1; i>=0; i--){
+        if(s>9){
+            v[i] = 9;
+            s-=9;
+        }else{
+            v[i] = s;
+            s = 0;
+        }
+    }
+
+    if(v[0] == 0){
+        v[0] = 1;
+        for(int i=1; i<m;i++){
+            if(v[i]>0){
+                v[i]--;
+                break;
             }
         }
-
-        cout << maax << " ";
     }
+
+    cout(v); cout << " ";
+
+    s = sum;
+    for(int i=0; i<m; i++){
+        if(s>9){
+            v[i] = 9;
+            s-=9;
+        }else{
+            v[i] = s;
+            s = 0;
+        }
+    }
+
+    cout(v);
+
 }
 
 signed main(){
-    int t; cin>>t;
-    while(t--) {
-        solve();
-        cout << "\n";
-    }
-
+    solve();
 }
