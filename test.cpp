4328ad7a4f42b3fd6326142c624de7974128ee72
ashish-um <ashishumrao632005@gmail.com>
2024-10-24 23:16:15 +0530

    C - Two Teams Composing

--- test.cpp
+++ test.cpp
@@ -1,69 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define cout(v) for(auto i:v){cout<<i<<"";}
+#define cout(v) for(auto i:v){cout<<i<<" ";}cout<<endl
 #define cin(v) for(auto &i:v){cin>>i;}
 #define qsort(v) sort(v.begin(), v.end())
 #define all(v) v.begin(), v.end()
 
+
 void solve(){
-    // 489C - Given Length and Sum of Digits
-    int m,s; cin>>m>>s;
-    if(m == 0 || s>9*m){
-        cout<<"-1 -1";
-        return;
-    }
-
-    if(m==1 && s==0){
-        cout << "0 0";
-        return;
-    }
-
-    if(m > 0 && s==0){
-        cout << "-1 -1";
-        return;
-    }
-    
-    vector<int> v(m);
-    int sum = s;
-
-    for(int i=m-1; i>=0; i--){
-        if(s>9){
-            v[i] = 9;
-            s-=9;
-        }else{
-            v[i] = s;
-            s = 0;
+    // C - Two Teams Composing 
+    int n; cin >> n;
+    // vector<int> v(n);
+    unordered_map<int, int> m;
+    int max_elem;
+    int mux = LONG_MIN;
+    for(int i{}; i<n; i++){
+        int temp; cin >> temp;
+        m[temp]++;
+        if(mux != max(mux, m[temp])){
+            max_elem = temp;
+            mux = max(mux, m[temp]);
         }
     }
 
-    if(v[0] == 0){
-        v[0] = 1;
-        for(int i=1; i<m;i++){
-            if(v[i]>0){
-                v[i]--;
-                break;
-            }
-        }
+    int my = min((int)(m.size()-1), m[max_elem]);
+    if(m[max_elem] > m.size()){
+        my = m.size();
     }
-
-    cout(v); cout << " ";
-
-    s = sum;
-    for(int i=0; i<m; i++){
-        if(s>9){
-            v[i] = 9;
-            s-=9;
-        }else{
-            v[i] = s;
-            s = 0;
-        }
-    }
-
-    cout(v);
-
+    cout << my;
 }
-
+ 
 signed main(){
-    solve();
+    int t; cin >>t;
+    while(t--){
+        solve();
+        cout << "\n";
+    }
 }
