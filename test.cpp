e6b53330c487f8ad047d5f13568cb4f702b184e5
ashish-um <ashishumrao632005@gmail.com>
2024-11-06 20:28:12 +0530

    B - Interesting drink

--- test.cpp
+++ test.cpp
@@ -7,20 +7,20 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 
 void solve(){
-    int n, l; cin >> n >> l;
-    vector<int> v(n); cin(v);
+    int n; cin >> n;
+    vector<int> x(n); cin(x);
+    qsort(x);
+    int q; cin >> q;
+    for(int i{}; i<q; i++){
+        int temp; cin >> temp;
+        auto it = upper_bound(all(x), temp);
 
-    qsort(v);
-
-    double max_diff = LONG_MIN;
-    for(int i=1; i<n; i++){
-        max_diff = max(max_diff, (double)v[i] - v[i-1]);
+        if(it != x.begin()){
+            cout << it - x.begin() << "\n";
+        }else{
+            cout << 0 << "\n";
+        }
     }
-    max_diff /= 2;
-    max_diff = max((double)v[0], max_diff);
-    max_diff = max((double)l-*v.rbegin(), max_diff);
-
-    cout << setprecision(10) << fixed << max_diff << endl;
 }
  
 signed main(){
