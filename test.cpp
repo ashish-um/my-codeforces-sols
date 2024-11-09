dce10afe80c09bcdb46ea24904772f103890e129
ashish-um <ashishumrao632005@gmail.com>
2024-11-09 16:14:08 +0530

    B - Worms

--- test.cpp
+++ test.cpp
@@ -7,46 +7,34 @@ using namespace std;
 #define all(v) v.begin(), v.end()
     
 void solve(){
-    string s; cin >> s;   
-    vector<pair<char, int>> v;
-
-    v.push_back(make_pair(s[0], 1));
-
-    int x = 1;
-    while(s[x] == v[0].first){
-        x++;
-        v[0].second++;
+    int n; cin >> n;
+    vector<int> v(n); cin(v);
+    for(int i=1; i<n; i++){
+        v[i] += v[i-1];
     }
 
-    int vIndex = 0;
+    vector<int> help(*v.rbegin()+1);
 
-    for(int i=x; i < s.length(); i++){
-        if(v[vIndex].first != s[i]){
-            v.push_back(make_pair(s[i], 1));
-            vIndex++;
-        }else{
-            v[vIndex].second++;
+    int count=1;
+    for(int i=0;i<*v.rbegin()+1;i++){
+        if(i>v[count-1]){
+            count++;
         }
+        help[i] = count;
     }
+    int m; cin>>m;
+    for(int i{}; i<m; i++){
+        int inp; cin >> inp;
 
-    int ans = LONG_MAX;
-    for(int i=1; i<v.size()-1; i++){
-        if(v[i-1].first - v[i+1].first != 0){
-            ans = min(ans, v[i].second+2);
-        }
+        cout << help[inp] << "\n";
     }
-
-    if(ans == LONG_MAX){
-        cout << 0;
-    }else cout << ans;
-
 }
     
 signed main(){
-    int t; cin >>t;
-    while(t--){
-        solve();
-        cout << "\n";
-    }
-    // solve();
+    // int t; cin >>t;
+    // while(t--){
+    //     solve();
+    //     cout << "\n";
+    // }
+    solve();
 }
